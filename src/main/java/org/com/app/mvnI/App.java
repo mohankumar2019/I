ead48baae529632e5800ba0cde51a949a906bf96
mohan <mohan.chanti4@gmail.com>
2019-07-24 12:19:16 +0530

    new changes

---
+++ src/main/java/org/com/app/mvnI/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mvnI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mvnI" );
+    }
+}
